ba7292246acfa17a3498378dcdb1acd32a7f48e6
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2023-11-10 11:42:09 +0200

    add new files

---
+++ 07_set/ex_1_1.py
@@ -0,0 +1,41 @@
+#
+# my_set = set(input() for _ in range(int(input())))
+# while my_set:
+#     print(my_set.pop())
+
+
+
+#
+# my_set = set()
+# for _ in range(int(input())):
+#     current_name = input()
+#     my_set.add(current_name)
+#
+#
+#
+# while my_set:
+#     print(my_set.pop())
+
+
+
+
+
+
+
+
+# n = int(input())
+# my_list = []
+# unic_list = []
+# for _ in range(n):
+#     current_name = input()
+#     my_list.append(current_name)
+# print(my_list)
+#
+# for i in range(len(my_list)):
+#     new_name = my_list[i]
+#     if new_name not in unic_list:
+#         unic_list.append(new_name)
+# print(unic_list)
+#
+# while unic_list:
+#     print(unic_list.pop())
